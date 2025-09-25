6e3480e6db83fedeefa9dde1a59b9ef7fcdf77bd
Prashik-Sasane <prashiksasane85@gmail.com>
2025-09-25 10:10:25 +0530

    add login page in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { SignupFormDemo } from './components/SignupFormDemo'
 import { NavbarDemo } from './components/NavbarDemo'
 import './App.css'
 
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<NavbarDemo />} />
+        <Route path="/signup" element={<SignupFormDemo />} />
       </Routes>
     </BrowserRouter>
   )
